85d4fcdff118802fdccd4cad198aa63db6ab0eac
VahanBG <vahan365105@gmail.com>
2022-06-11 18:03:03 +0400

    next commit

--- main.cpp
+++ main.cpp
@@ -5,11 +5,10 @@
  {
     
     MyGap_Buffer aaa("vahan");
-	 
 	 aaa.Print_buf();
 	 aaa.insert_string("vahan", 0);
-	aaa.Print_buf();
-	aaa.erase(10 , 14);
+	 aaa.Print_buf();
+	 aaa.erase(10 , 14);
 	 aaa.Print_buf();
 	 aaa.erase( 0 , 1);
 	 aaa.Print_buf();
